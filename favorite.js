84e960b69b9356752dc00ac7dfd9970182d2ea5b
Muhammad <mhmdmt865@gmail.com>
2024-07-12 11:15:55 +0700

    Menambahkan fitur Favorites --Muhammad

---
+++ favorite.js
@@ -0,0 +1,37 @@
+const products = JSON.parse(localStorage.getItem("products"));
+let favorites= [];
+
+products.forEach(product=>{
+    if(product.fav == true){
+        favorites.push(product)
+    }
+})
+
+
+function stockStatus(title){
+    const item = favorites.find(product=>product.title == title);
+    return item.stock>0 ? 'In Stock':'Out of stock';
+}
+
+let content = "";
+const container = document.querySelector(".favorite-content")
+function loadFavorite(){
+    favorites.forEach(favorite=>{
+        content += '<div class="tr">';
+        content += '<div class="products">';
+        content += `<img src="${favorite.imageUrl}" alt="">`
+        content += `<div> <h1>${favorite.category}</h1> <h3>${favorite.title}</h3></div>`;
+        content += `</div>`
+        content += `<h1>Rp${favorite.price}</h1>`
+        content += `<h1>${stockStatus(favorite.title)}</>`
+        content += `<h1>${favorite.date}</h1>`
+        content += ` <div class="button">`;
+        content += `<button>Add To Cart </button>`
+        content += `</div> </div>`;
+    });
+    container.innerHTML = content;
+}
+
+document.onload = loadFavorite();
+
+
--- script.js
+++ script.js
@@ -29,60 +29,66 @@ setInterval(function(){
 
 
 // data products
-// let products = [
-//   {
-//     category : 'cookies',
-//     imageUrl : 'asset/cookies-chocolate.png',
-//     title : 'Chocolate',
-//     desc : 'Rich and indulgent, offering a deep cocoa flavor thats perfect for satisfying sweet cravings.',
-//     stock :  0,
-//     price : 3000
-//   },
-//   {
-//     category : 'cookies',
-//     imageUrl : 'asset/cookies-cheese.png',
-//     title : 'Cheese',
-//     desc : 'Combining the savory taste of cheese with a hint of sweetness, creating a unique and addictive snack.',
-//     stock :  0,
-//     price : 3000
-//   },
-//   {
-//     category : 'cookies',
-//     imageUrl : 'asset/cookies-matcha.png',
-//     title : 'Matcha',
-//     desc : 'Features the distinct, earthy flavor of green tea, providing a subtly sweet and slightly bitter taste experience.',
-//     stock :  0,
-//     price : 3000
-//   },
-//   {
-//     category : 'risoles',
-//     imageUrl : 'asset/risoles-mayonaise.png',
-//     title : 'Mayonaise',
-//     desc : 'Rich and indulgent, offering a deep cocoa flavor thats perfect for satisfying sweet cravings.',
-//     stock :  0,
-//     price : 3000
-//   },
-//   {
-//     category : 'risoles',
-//     imageUrl : 'asset/risoles-ragout.png',
-//     title : 'Ragout',
-//     desc : 'Rich and indulgent, offering a deep cocoa flavor thats perfect for satisfying sweet cravings.',
-//     stock :  0,
-//     price : 3000
-//   },
-//   {
-//     imageUrl : 'asset/risoles-spicy.png',
-//     title : 'Spicy',
-//     desc : 'Rich and indulgent, offering a deep cocoa flavor thats perfect for satisfying sweet cravings.',
-//     stock :  0,
-//     price : 3000
-//   }
-// ]
+let Default_products = [
+  {
+    category : 'cookies',
+    imageUrl : 'asset/cookies-chocolate.png',
+    title : 'Chocolate',
+    desc : 'Rich and indulgent, offering a deep cocoa flavor thats perfect for satisfying sweet cravings.',
+    stock :  0,
+    price : 3000,
+    fav : false
+  },
+  {
+    category : 'cookies',
+    imageUrl : 'asset/cookies-cheese.png',
+    title : 'Cheese',
+    desc : 'Combining the savory taste of cheese with a hint of sweetness, creating a unique and addictive snack.',
+    stock :  0,
+    price : 3000,
+    fav : false
+  },
+  {
+    category : 'cookies',
+    imageUrl : 'asset/cookies-matcha.png',
+    title : 'Matcha',
+    desc : 'Features the distinct, earthy flavor of green tea, providing a subtly sweet and slightly bitter taste experience.',
+    stock :  0,
+    price : 3000,
+    fav : false
+  },
+  {
+    category : 'Risoles',
+    imageUrl : 'asset/Risoles-mayonaise.png',
+    title : 'Mayonaise',
+    desc : 'Rich and indulgent, offering a deep cocoa flavor thats perfect for satisfying sweet cravings.',
+    stock :  0,
+    price : 3000,
+    fav : false
+  },
+  {
+    category : 'Risoles',
+    imageUrl : 'asset/risoles-ragout.png',
+    title : 'Ragout',
+    desc : 'Rich and indulgent, offering a deep cocoa flavor thats perfect for satisfying sweet cravings.',
+    stock :  0,
+    price : 3000,
+    fav : false
+  },
+  {
+    category : 'Risoles',
+    imageUrl : 'asset/risoles-spicy.png',
+    title : 'Spicy',
+    desc : 'Rich and indulgent, offering a deep cocoa flavor thats perfect for satisfying sweet cravings.',
+    stock :  0,
+    price : 3000,
+    fav : false
+  }
+]
 
-// localStorage.setItem("products", JSON.stringify(products));
+localStorage.setItem("products", JSON.stringify(Default_products));
 
 let products = JSON.parse(localStorage.getItem("products"));
-let cart =  [];
 //add to cart function
 function addCart(item){
     let product_cart = products.find(product=>product.title == item);
@@ -94,28 +100,58 @@ function addCart(item){
     }
     
 }
+//end ad to cart
+
+//Adding item to favorite localsotrage
+function like(title){
+    products.forEach(product=>{
+      if(product.title== title){
+        product.fav = true ;
+      }
+      localStorage.setItem("products",JSON.stringify(products));
+    })
+}
+
+//end like product
 
 // Start load products
 let productsContent = "";
 const containerProducts =document.querySelector(".products");
+
 function showProducts(){
     products.forEach(product => {
+        let index = 0;
         productsContent += `<div class="card-product">`;
-        
         productsContent += `<div class="photo">`;
-        productsContent += `<i class="fa-solid fa-heart"></i>`;
+        productsContent += `<i onclick="" class="fa-solid fa-heart"></i>`;
         productsContent += `<img src="${product.imageUrl}" alt="">`;
         productsContent += `</div>`;
-
         productsContent += `<div class="desc-product">`;
         productsContent += `<h1>${product.title}</h1>`;
         productsContent += `<p>${product.desc}</p>`;
         productsContent += `<button onclick="addCart('${product.title}')">Add To cart</button>`
         productsContent += `</div>`;
         productsContent += `</div>`;     
+        index++;
     });
     containerProducts.innerHTML = productsContent;
 }
 document.onload = showProducts();
+
+const likeBtn = document.querySelectorAll('.fa-heart');
+for(let i = 1; i<likeBtn.length;i++){
+  
+  likeBtn[i].addEventListener('click',function(){
+    if(this.className.includes('liked')){
+      products[i-1].fav =  false;
+      this.classList.remove('liked');
+    }
+    else{
+      products[i-1].fav =  true;
+      this.classList.add("liked")
+    };
+    localStorage.setItem("products", JSON.stringify(products)); 
+  });
+}
 // end load products
 
